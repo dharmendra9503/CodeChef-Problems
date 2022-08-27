944fd9fb2899b5f2e557b69e6b1fadf9373471a8
Dharmendra <dharmendraprajapati9503@gmail.com>
2022-08-27 18:24:28 +0530

    CodeChef Program

--- Greater_Average.java
+++ Greater_Average.java
@@ -1,4 +1,23 @@
 package JavaProgramming2022;
 
+import java.util.Scanner;
+
 public class Greater_Average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        while(T-- > 0) {
+            int A = sc.nextInt(), B = sc.nextInt(), C = sc.nextInt();
+            float avg = (float) (A + B) / 2;
+
+            if(avg > C){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
 }
